631ed48c1511cb4e9a0037a88c27a52a8549e112
kraken00ch <115397496+kraken00ch@users.noreply.github.com>
2023-12-20 02:20:49 -0800

    updates to app.py and index.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-from JMN_Planet_Calc import get_user_weight, get_planet_selection, calculate_weight_on_planet
+# app.py (Flask backend)
+from flask import Flask, render_template, request, jsonify
+from JMN_Planet_Calc.py import calculate_weight_on_planet
 
 app = Flask(__name__)
 
@@ -9,10 +10,10 @@ def index():
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    userweight = get_user_weight()
-    planet = get_planet_selection()
+    userweight = float(request.form['userweight'])
+    planet = int(request.form['planet'])
     result = calculate_weight_on_planet(userweight, planet)
-    return render_template('result.html', userweight=userweight, result=result)
+    return jsonify({'userweight': userweight, 'result': result})
 
 if __name__ == '__main__':
     app.run(debug=True)
