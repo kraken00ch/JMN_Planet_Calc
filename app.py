dc7ba1759bd15117fc408c2a89eb0e94cfe79cdb
kraken00ch <115397496+kraken00ch@users.noreply.github.com>
2023-12-26 22:46:07 -0800

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
